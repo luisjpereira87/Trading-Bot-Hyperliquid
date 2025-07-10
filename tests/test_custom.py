be2b58e2d2b070bf800bdeb98aa922d95207dfc8
Luís Pereira <luisjpereira87@gmail.com>
2025-07-11 00:49:36 +0100

    Fix file

--- tests/test_custom.py
+++ tests/test_custom.py
@@ -9,9 +9,9 @@ class TestCustom:
         pass
 
     async def run(self):
-        print(f" TESTE is_opposite_side with params {Signal.SELL} buy  result => {TradingHelpers.is_opposite_side(Signal.SELL, Signal.from_str("buy"))}")
+        print(f" TESTE is_opposite_side with params {Signal.SELL} buy  result => {TradingHelpers.is_opposite_side(Signal.SELL, Signal.from_str('buy'))}")
         print(f" TESTE get_opposite_side with params {Signal.SELL} result => {TradingHelpers.get_opposite_side(Signal.SELL)}")
         print(f" TESTE get_opposite_side with params {Signal.BUY} result => {TradingHelpers.get_opposite_side(Signal.BUY)}")
-        print(f" TESTE is_signal_opposite_position with params {Signal.SELL} result => {TradingHelpers.is_signal_opposite_position(Signal.SELL, Signal.from_str("sell"))}")
-        print(f" TESTE is_signal_opposite_position with params {Signal.BUY} result => {TradingHelpers.is_signal_opposite_position(Signal.BUY, Signal.from_str("sell"))}")
+        print(f" TESTE is_signal_opposite_position with params {Signal.SELL} result => {TradingHelpers.is_signal_opposite_position(Signal.SELL, Signal.from_str('sell'))}")
+        print(f" TESTE is_signal_opposite_position with params {Signal.BUY} result => {TradingHelpers.is_signal_opposite_position(Signal.BUY, Signal.from_str('sell'))}")
     
